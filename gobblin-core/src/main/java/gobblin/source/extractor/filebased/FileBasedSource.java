adb9a812a89efb0803e4a914798a01e133effc83
Joel Baranick <joel.baranick@ensighten.com>
2017-04-13 10:54:25 -0700

    Fix spacing

--- gobblin-core/src/main/java/gobblin/source/extractor/filebased/FileBasedSource.java
+++ gobblin-core/src/main/java/gobblin/source/extractor/filebased/FileBasedSource.java
@@ -104,11 +104,11 @@ public abstract class FileBasedSource<S, D> extends AbstractSource<S, D> {
       if (previousSourceState.contains(ConfigurationKeys.SOURCE_FILEBASED_FS_SNAPSHOT)) {
         prevFsSnapshot = previousSourceState.getPropAsSet(ConfigurationKeys.SOURCE_FILEBASED_FS_SNAPSHOT);
       } else if (state.getPropAsBoolean(ConfigurationKeys.SOURCE_FILEBASED_FS_PRIOR_SNAPSHOT_REQUIRED,
-                ConfigurationKeys.DEFAULT_SOURCE_FILEBASED_FS_PRIOR_SNAPSHOT_REQUIRED)) {
+            ConfigurationKeys.DEFAULT_SOURCE_FILEBASED_FS_PRIOR_SNAPSHOT_REQUIRED)) {
           // If a previous job exists, there should be a snapshot property.  If not, we need to fail so that we
           // don't accidentally read files that have already been processed.
           throw new RuntimeException(String.format("No '%s' found on state of prior job '%s'",
-                  ConfigurationKeys.SOURCE_FILEBASED_FS_SNAPSHOT, previousSourceState.getId()));
+              ConfigurationKeys.SOURCE_FILEBASED_FS_SNAPSHOT, previousSourceState.getId()));
       }
     }
 
